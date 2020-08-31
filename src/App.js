5fa6659b4869549e7a9ffcf754f576478718e613
Jeroen Van Soest <jeroen.van.soest@vub.be>
2020-08-31 20:22:17 +0200

    added mock site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const maps = [
+    {
+      map: "ascent",
+      role: "attack",
+      explanation: "full charge one bounce",
+      picture:
+        "https://images.unsplash.com/photo-1543201245-c9031909fe6f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    },
+    {
+      map: "split",
+      role: "attack",
+      explanation: "full charge one bounce",
+      picture:
+        "https://images.unsplash.com/photo-1543201245-c9031909fe6f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    },
+    {
+      map: "bind",
+      role: "defense",
+      explanation: "full charge one bounce",
+      picture:
+        "https://images.unsplash.com/photo-1543201245-c9031909fe6f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    },
+    {
+      map: "haven",
+      role: "defense",
+      explanation: "full charge one bounce",
+      picture:
+        "https://images.unsplash.com/photo-1543201245-c9031909fe6f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    },
+  ];
+
+  const [map, setMap] = useState("split");
+  const [role, setRole] = useState("attack");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Map
+      <select value={map} onChange={(e) => setMap(e.target.value)}>
+        <option value="split">Split</option>
+        <option value="haven">Haven</option>
+        <option value="bind">Bind</option>
+        <option value="ascent">Ascent</option>
+      </select>
+      Role
+      <select value={role} onChange={(e) => setRole(e.target.value)}>
+        <option value="attack">ATTACK</option>
+        <option value="defense">DEFENSE</option>
+      </select>
+      {role + " " + map}
+      {maps
+        .filter((e) => e.map === map && e.role === role)
+        .map((map) => {
+          return (
+            <div>
+              <h1>{map.name}</h1>
+              <p>
+                {map.role}
+                {": "}
+                {map.explanation}
+              </p>
+              <img width="500" src={map.picture} alt={map.name}></img>
+            </div>
+          );
+        })}
     </div>
   );
 }
