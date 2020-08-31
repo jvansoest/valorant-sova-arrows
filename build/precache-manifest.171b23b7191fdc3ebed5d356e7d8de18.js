90986cc8df6816289e343e5b3f09fd9fad2ff134
Jeroen Van Soest <jeroen.van.soest@vub.be>
2020-08-31 21:09:46 +0200

    added build

---
+++ build/precache-manifest.171b23b7191fdc3ebed5d356e7d8de18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f42e2b561b8e58ab70809401b554af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a40b35d8a6d60afca74f",
+    "url": "/static/js/2.84bb425b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84bb425b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df84fe217cabcbe6434c",
+    "url": "/static/js/main.fbc9e80a.chunk.js"
+  },
+  {
+    "revision": "2a0f45ea98a24f814a95",
+    "url": "/static/js/runtime-main.d50e7529.js"
+  }
+]);
